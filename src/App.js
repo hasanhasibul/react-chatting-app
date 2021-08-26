8db971addf62a25934c23f8058da8a0972a789b3
hasanhasibul <hasanhasibul395@gmail.com>
2021-08-27 01:25:15 +0600

    chat application complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+import { ChatEngine } from 'react-chat-engine';
 import './App.css';
+import ChatFeed from './components/ChatFeed/ChatFeed';
 
 function App() {
   return (
     <div className="App">
-      hello
+      <ChatEngine
+        projectID='963211d1-95c7-4b3f-b361-ae10b351c7b5'
+        userName='hasib'
+        userSecret='123123'
+        height="100vh"
+        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} ></ChatFeed>}
+      />
     </div>
   );
 }
